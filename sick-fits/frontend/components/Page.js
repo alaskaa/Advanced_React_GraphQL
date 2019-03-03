9626b6d773bd5ff3bc55d0778efec34fd5b39aec
alaskaa <sibylle.sehl@googlemail.com>
2019-03-03 19:52:28 +0100

    Added styled components.

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import Meta from './Meta';
-import Header from './Header';
+import React from "react";
+import Meta from "./Meta";
+import Header from "./Header";
+import styled from "styled-components";
+
+const MyButton = styled.button`
+  background: red;
+  font-size: ${props => (props.huge ? "100px" : "50px")};
+  span {
+    font-size: 100px;
+  }
+`;
 
 class Page extends React.Component {
-    render() {
-        return (
-            <div>
-                <Meta />
-                <Header />
-                {this.props.children}
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Meta />
+        <Header />
+        <MyButton huge>hALLO</MyButton>
+        {this.props.children}
+      </div>
+    );
+  }
 }
 
 export default Page;
