0b7c8a121cdb24e4577be34146a80cf1d0d8c87d
alaskaa <sibylle.sehl@googlemail.com>
2019-03-03 20:27:13 +0100

    Create global theme and update header

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,25 +1,31 @@
 import React from "react";
+import styled, { ThemeProvider, injectGlobal } from "styled-components";
+import theme from "../theme";
+
 import Meta from "./Meta";
 import Header from "./Header";
-import styled from "styled-components";
 
-const MyButton = styled.button`
-  background: red;
-  font-size: ${props => (props.huge ? "100px" : "50px")};
-  span {
-    font-size: 100px;
-  }
+const StyledPage = styled.div`
+  background: white;
+  color: ${({ theme }) => theme.black};
+`;
+
+const Inner = styled.div`
+  max-width: ${({ theme }) => theme.maxWidth};
+  margin: 0 auto;
+  padding: 2rem;
 `;
 
 class Page extends React.Component {
   render() {
     return (
-      <div>
-        <Meta />
-        <Header />
-        <MyButton huge>hALLO</MyButton>
-        {this.props.children}
-      </div>
+      <ThemeProvider theme={theme}>
+        <StyledPage>
+          <Meta />
+          <Header />
+          <Inner>{this.props.children}</Inner>
+        </StyledPage>
+      </ThemeProvider>
     );
   }
 }
