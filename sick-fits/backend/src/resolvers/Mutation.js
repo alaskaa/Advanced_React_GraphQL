0345eefdef9565d1b35c505f83cd7a42b7eaa5ae
alaskaa <sibylle.sehl@googlemail.com>
2019-04-16 21:21:28 +0200

    [TASK] add Item to datamodel, pull new data and update resolvers to match schema

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,8 +1,23 @@
 const Mutations = {
-  createDog(parent, args, ctx, info) {
-    // createDog
-    console.log(args);
+  async createItem(parent, args, ctx, info) {
+    // TODO - check if they are loggin in
+
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
+
+    return item;
   }
+
+  // createDog(parent, args, ctx, info) {
+  //   // createDog
+  //   console.log(args);
+  // }
 };
 
 module.exports = Mutations;
