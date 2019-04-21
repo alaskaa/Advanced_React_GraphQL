e3f644b5d4602f6999d3a65e3b97adbddf3f07bb
alaskaa <sibylle.sehl@googlemail.com>
2019-04-21 21:11:14 +0200

    [TASK] flesh out _app.js with Apollo

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,18 +1,35 @@
-import App, { Container } from 'next/app';
-import Page from '../components/Page';
+import App, { Container } from "next/app";
+import Page from "../components/Page";
+import { ApolloProvider } from "react-apollo";
+import withData from "../lib/withData";
 
 class MyApp extends App {
-    render() {
-        const { Component } = this.props;
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
 
-        return (
-            <Container>
-                <Page>
-                    <Component />
-                </Page>
-            </Container>
-        )
+    // crawls all pages and gets data to us
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
     }
+    // this exposes query to the user
+    pageProps.query = ctx.query;
+    return { pageProps };
+
+    // by returning sth here we make it accessible via props down in render
+  }
+  render() {
+    const { Component, apollo, pageProps } = this.props;
+
+    return (
+      <Container>
+        <ApolloProvider client={apollo}>
+          <Page>
+            <Component {...pageProps} />
+          </Page>
+        </ApolloProvider>
+      </Container>
+    );
+  }
 }
 
-export default MyApp;
+export default withData(MyApp);
